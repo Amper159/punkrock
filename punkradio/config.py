b817cf9d449ea8e54d81f6f6d3dd44606c001c68
Amper159 <lampart.m1305@gmail.com>
2025-11-20 20:31:22 +0100

    ladění

--- punkradio/config.py
+++ punkradio/config.py
@@ -5,6 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
+    DEBUG = False
     SECRET_KEY = os.getenv('SECRET_KEY', 'supersecretkey')
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "instance", "app.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
